12baa345babc02174b412190a6005a47e522adeb
shengyumeng@gmail.com <shengyumeng@gmail.com>
2024-12-07 15:40:13 -0800

    document management redone

--- services/document_management/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  
-
-class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://localhost:27017")
-    DOCUMENT_MANAGEMENT_PORT = os.getenv("DOCUMENT_MANAGEMENT_PORT", "8001")
--- services/document_management/app/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from .config import Config
-
-database_name = 'tbd'
-
-client = MongoClient(Config.DATABASE_URL)
-db = client[database_name]  # Database name
-documents_collection = db["documents"]  # Collection name
